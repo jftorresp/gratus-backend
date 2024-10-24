551fa259158366726e95a60d582f20f2a64ed27c
Juan Felipe Torres Piza <jf.torresp@uniandes.edu.co>
2024-10-24 16:28:46 -0500

    Added logs

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,7 @@
-if(process.env.NODE_ENV === 'production'){
+if (process.env.NODE_ENV === 'production') {
     module.exports = require('./prod');
-}else{
+    console.log("Server configured fro production");
+} else {
     module.exports = require('./dev');
+    console.log("Server configured fro develop");
 }
